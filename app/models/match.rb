51522b35650cbdd7fd9481aa465a954d8aade161
Iyanu Adelekan <iyanuadelekan@gmail.com>
2017-04-01 09:30:53 +0100

    Fixed issue with matching

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,5 +2,7 @@ class Match < ApplicationRecord
 	belongs_to :user
 	belongs_to :matched_user, class_name: "User"
 
+	validates :user_id, :matched_user_id, :completion_status
+
 	scope :ordered, -> { order('created_at desc') }
 end
