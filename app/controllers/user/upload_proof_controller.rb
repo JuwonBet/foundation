4f63af35b66fe633a317792116c0d35c0313507b
Iyanu Adelekan <iyanuadelekan@gmail.com>
2017-04-04 06:35:18 +0100

    Done changes to flow

--- app/controllers/user/upload_proof_controller.rb
+++ app/controllers/user/upload_proof_controller.rb
@@ -8,29 +8,25 @@ class User::UploadProofController < ApplicationController
 
   def create
 
-    if !%w(jpg jpeg).include?(params[:upload_file].original_filename.split('.')[1])
-      flash[:error_message] = 'Invalid file format. You can only upload jpeg files.'
+    if Match.exists?(id: params[:match_id], user_id: current_user.id)
+      #extension = params[:upload_file].original_filename.split('.')[1]
+      #name = params[:match_id] << '.' << extension #params[:upload][:file].original_filename
+      #directory = "#{Rails.root}/public/images/proof_uploads"
+      #path = File.join(directory, name)
+      #File.open(path, 'wb') { |f| f.write(params[:upload_file].read) }
+
+      proof_upload = ProofUpload.new
+      proof_upload.match = Match.find(params[:match_id])
+      proof_upload.account_name = params[:user_name]
+      proof_upload.phone_number = params[:phone_number]
+      proof_upload.account_number = params[:account_number]
+      #proof_upload.proof_url = name
+      proof_upload.save
+
+      redirect_to '/user/dashboard'
+
     else
-      if Match.exists?(id: params[:match_id], user_id: current_user.id)
-        extension = params[:upload_file].original_filename.split('.')[1]
-        name = params[:match_id] << '.' << extension #params[:upload][:file].original_filename
-        directory = "#{Rails.root}/public/images/proof_uploads"
-        path = File.join(directory, name)
-        File.open(path, 'wb') { |f| f.write(params[:upload_file].read) }
-
-        proof_upload = ProofUpload.new
-        proof_upload.match = Match.find(params[:match_id])
-        proof_upload.account_name = params[:user_name]
-        proof_upload.phone_number = params[:phone_number]
-        proof_upload.account_number = params[:account_number]
-        proof_upload.proof_url = name
-        proof_upload.save
-
-        redirect_to '/user/dashboard'
-
-      else
-        flash[:error_message] = 'Invalid upload details. Please cross check form'
-      end
+      flash[:error_message] = 'Invalid upload details. Please cross check form'
     end
 
   end
